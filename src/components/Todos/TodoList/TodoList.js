289fa7b1d1cc98a90de6f4e0625425663801b32f
Kapakahi <rubbishman@gmail.com>
2022-11-15 21:53:23 +0900

    set up todo functionality

---
+++ src/components/Todos/TodoList/TodoList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import TodoItem from "../TodoItem/TodoItem";
+import "./TodoList.css";
+
+const todoList = (props) => {
+  return (
+    <ul className="todo-list">
+      {props.items.map((todo) => (
+        <TodoItem key={todo.id} id={todo.id} onDelete={props.onDeleteItem}>
+          {todo.text}
+        </TodoItem>
+      ))}
+    </ul>
+  );
+};
+
+export default todoList;
